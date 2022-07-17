b07e5f4ccabd187d85ab5cea0f582578c8b425e5
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-17 22:36:41 +0300

    render country

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-export function fetchCountries(name) {
+export function fetchCountries() {
     const url = 'https://restcountries.com/v2/all?fields=name,capital,population,flags,languages';
 
     return fetch(url).then(responce => responce.json());
--- src/index.js
+++ src/index.js
@@ -10,25 +10,32 @@ import fetchCountries from "./fetchCountries";
 
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.country-list');
+const containerEl = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
-// розмітка списку країн при пошуку    не треба
-// const markup = createItemsMarkup(items);
-// listEl.innerHTML = markup;
+// розмітка списку країн при пошуку
+function createItemsMarkup(items) {
+return items.map(item => `<li>${item.name}</li>`).join();
+}
 
-// function createItemsMarkup(items) {
-// return items.map(item => `<li>${item.name}</li>`).join();
-// }
-
-inputEl.addEventListener('input', debounce(onSearch, 300));
+inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
     const filter = event.target.value.toLowerCase().trim();
     console.log(filter)
 
-    // const filteredItems = countries.filter(item => item.toLowerCase().includes(filter))
+    const filteredItems = countries.filter(item => item.toLowerCase().includes(filter))
 
-    // const markup = createItemsMarkup(filteredItems);
-    // listEl.innerHTML = markup;
+    const markup = createItemsMarkup(filteredItems);
+    listEl.innerHTML = markup;
 }
-// fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages').then(responce => responce.json()).then(console.log);
+
+fetchCountries()
+.then(renderCountryInfo)
+.catch(error => console.log(error));
+// Notiflix.Notify.failure(`"Oops, there is no country with that name"`)
+
+function renderCountryInfo(country) {
+    const markup = countryInfo(country);
+    containerEl.innerHTML = markup;
+}
